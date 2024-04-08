eb79e70d3515c2fb27a8d46cad2fb1ac6729847c
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-08 05:21:45 +0300

    add home path

--- instaclone/src/App.jsx
+++ instaclone/src/App.jsx
@@ -2,12 +2,14 @@ import "./styles/colors.css"
 import "./styles/uitilities.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Authentication from "./pages/Authentication";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route  path="/" element={<Authentication />}/>
+        <Route  path="/home" element={<Home />}/>
       </Routes>
     </BrowserRouter>
       

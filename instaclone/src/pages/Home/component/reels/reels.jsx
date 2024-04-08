9c07319c6fb87de1c66480f53c60180e72dedf4e
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-08 05:20:39 +0300

    add rerls

---
+++ instaclone/src/pages/Home/component/reels/reels.jsx
@@ -0,0 +1,16 @@
+import './reels.css'
+
+const Reels = ()=>{
+    return (
+        <div className="reels flex center">
+            <div className="reel flex center">
+                <div className="child"></div>
+                <div className="child"></div>
+                <div className="child"></div>
+                <div className="child"></div>
+                <div className="child"></div>
+            </div>
+        </div>
+    )
+}
+export default Reels

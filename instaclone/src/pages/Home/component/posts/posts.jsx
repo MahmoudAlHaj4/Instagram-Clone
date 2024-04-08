85b69bb5dbbd42f16d4f0a72296a068457dfba5e
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-08 06:07:57 +0300

    add posts

--- instaclone/src/pages/Home/component/posts/posts.jsx
+++ instaclone/src/pages/Home/component/posts/posts.jsx
@@ -1,8 +1,34 @@
-
+import './posts.css'
 
 const Posts = ()=>{
     return (
-        <div>Hello posts</div>
+        <div className="Posts-page flex center">
+             <div className="post">
+                <div className='head flex'>
+                    <div className='user-img'>
+                        <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=1200&s=1" alt="User" />
+                    </div>
+                    <div className='name'>
+                        <p>Username</p>
+                    </div>
+                </div>
+                <div className='img-container'>
+                    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=1200&s=1" alt="Post" />
+                </div>
+                <div className="icons">
+                   
+                </div>
+                <div className="caption">
+                    
+                </div>
+                <div className="comments">
+                    
+                </div>
+                <div className="add-comment">
+                    
+                </div>
+            </div>
+        </div>
     )
 }
 

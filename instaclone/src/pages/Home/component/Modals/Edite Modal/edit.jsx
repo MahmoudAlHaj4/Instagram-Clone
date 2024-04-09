b497b4b44f93bfd550f24c37deabac9ca4b214cd
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-09 21:02:06 +0300

    add edit modal

--- instaclone/src/pages/Home/component/Modals/Edite Modal/edit.jsx
+++ instaclone/src/pages/Home/component/Modals/Edite Modal/edit.jsx
@@ -1,8 +1,27 @@
-const Edit =  ()=>{
+import "./edit.css"
+
+
+const Edit =  ({ isOpen, onClose })=>{
     return (
-        <div>
-            hello
-        </div>
+        <>
+        {isOpen && (
+            <div className="edit-modal-overlay">
+                <div className="edit-modal">
+                    <div className="edit-modal-content">
+                        <h2>Edit Profile</h2>
+                        <input type="text"  placeholder="Username"/>
+                        <input type="text" placeholder="Email"/>
+                        <textarea name="" id="" cols="30" rows="10" placeholder="bio"></textarea>
+                        <div className="modal-btn flex ">
+                            <button className="save"onClick={onClose}>submit</button>
+                            <button onClick={onClose}>Close</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        )}
+    </>
     )
 }
 

b3766888905a221fee549eb00ac0734ef96b3804
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-11 16:33:07 +0300

    request nethod in signup

--- instaclone/src/pages/Authentication/components/Signup.jsx
+++ instaclone/src/pages/Authentication/components/Signup.jsx
@@ -1,27 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import { sendRequest } from "../../../core/tools/sendRequest";
+import { requestMethods } from "../../../core/enums/requestsMethods";
+import { useState } from "react";
 
 const SignupForm = ({ setIsLogin }) => {
+  const [Credential, setCredential] = useState({})
+  const navigate = useNavigate();
+
     return (
       <div className="sign">
         <div className="signup-form flex column">
           <div className="heading flex center">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYkA4eLYzO589zptEK11phwXKTnAoqtNpUnXDdVOPEQw&s" alt="" />
+          <img src="https://as2.ftcdn.net/v2/jpg/03/97/48/01/1000_F_397480131_ifXqWNKVteOhczWDJBeODrnMIbVcVp13.jpg" alt="" />
+
           </div>
           <div className="links flex center column">
             <p>Sign up to see photos and videos from your friends.</p>
             <button>continue with Facebook</button>
             <button>continue with Google</button>
             <span className="flex">
-              <hr className="line" />
+              
               OR
-              <hr className="line" />
+             
             </span>
           </div>
           <div className="s-inputs flex column center">
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Password" />
-            <input type="text" placeholder="Confirm Password" />
-            <div className=" pp flex column center">
+            <input type="text" placeholder="Email" onChange={(e)=>{
+              setCredential({...Credential,name: e.target.value})
+            }} />
+
+            <input type="text" placeholder="Email" onChange={(e)=>{
+              setCredential({...Credential,email: e.target.value})
+            }}/>
+
+            <input type="text" placeholder="Password" onChange={(e)=>{
+              setCredential({...Credential,password: e.target.value})
+            }}/>
+            
+            <div className=" pp ">
               <p>
                 People who use our service may have uploaded your contact
                 information to Instagram.
@@ -31,7 +47,26 @@ const SignupForm = ({ setIsLogin }) => {
                 <span>Privacy Policy and Cookies Policy .!</span>
               </p>
             </div>
-            <button>Sign up</button>
+            <button
+            onClick={async(e)=>{
+              const formatedData = {
+                ...Credential
+              }
+              const res = await sendRequest(
+                requestMethods.POST,
+                '/register',
+                formatedData
+              )
+              if(res.data.status === 'success'){
+                localStorage.setItem('token', res.data.token)
+                console.log(res.data.token)
+                setIsLogin(true)
+              }
+              else{
+                
+              }
+            }}
+            >Sign up</button>
           </div>
           <div className="link flex center">
             <p>

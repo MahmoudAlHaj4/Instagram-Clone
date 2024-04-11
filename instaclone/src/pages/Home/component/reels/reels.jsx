d08dfa03433bef4e9dbfacbac60b91495de7bb08
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-11 16:38:32 +0300

    add reels

--- instaclone/src/pages/Home/component/reels/reels.jsx
+++ instaclone/src/pages/Home/component/reels/reels.jsx
@@ -1,14 +1,24 @@
+import Posts from '../posts/posts'
+import SideBar from '../sidebar/sidebar'
 import './reels.css'
+import Suggestions from './suggestions'
+import { useState } from 'react'
 
 const Reels = ()=>{
+    const [posts,setPosts] = useState([])
     return (
-        <div className="reels flex center">
-            <div className="reel flex center">
-                <div className="child"></div>
-                <div className="child"></div>
-                <div className="child"></div>
-                <div className="child"></div>
-                <div className="child"></div>
+        <div className="reels flex">
+            <div className="leftside">
+                <div className='reel-pos'>
+                    
+                        <Posts />
+                    
+                </div>
+            </div>
+            <div className="rightside">
+                <Suggestions />
+                
+               
             </div>
         </div>
     )

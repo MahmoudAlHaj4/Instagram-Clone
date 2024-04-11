44b8aa061efe6ed69f3f4983f7daad706d5a45bd
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-11 16:32:44 +0300

    request method at login

--- instaclone/src/pages/Authentication/components/LoginForm.jsx
+++ instaclone/src/pages/Authentication/components/LoginForm.jsx
@@ -1,17 +1,49 @@
 import { useNavigate } from "react-router-dom";
+import { sendRequest } from "../../../core/tools/sendRequest";
+import { requestMethods } from "../../../core/enums/requestsMethods";
+import { useState } from "react";
+
 
 const LoginForm = ({ setIsLogin }) => {
+  const [Credential, setCredential] = useState({})
   const navigate = useNavigate();
+  const [error, setError] = useState('');
 
     return (
       <div className="login flex column center">
         <div className="heading flex center">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYkA4eLYzO589zptEK11phwXKTnAoqtNpUnXDdVOPEQw&s" alt="" />
+          <img src="https://as2.ftcdn.net/v2/jpg/03/97/48/01/1000_F_397480131_ifXqWNKVteOhczWDJBeODrnMIbVcVp13.jpg" alt="" />
         </div>
         <div className="inputs flex column center">
-          <input type="text" placeholder="Email" />
-          <input type="text" placeholder="Password" />
-          <button onClick={() => navigate('/home')}>Login</button>
+          <input type="text" placeholder="Email" onChange={(e) => {
+                setCredential({ ...Credential, email: e.target.value });
+              }} />
+          <input type="text" placeholder="Password" onChange={(e) => {
+                setCredential({ ...Credential, password: e.target.value });
+              }} />
+          <button  onClick={async (e) => {
+                e.preventDefault();
+                const formatedData = {
+                  ...Credential,
+                };
+                try{
+                  const res = await sendRequest(
+                  requestMethods.POST,
+                  "/login",
+                  formatedData
+                );
+                console.log("Response data:", res.data);
+                if (res.data.status === "success" ) {
+                  localStorage.setItem("Bearer", res.data.authorization.token);
+                  navigate("/home");
+              }
+                }catch(error){
+                  console.error("Login error:", error);
+                  setError(error.response.data.message);
+                }
+                
+              }}>Login</button>
+              {error && <p style={{ color: 'red' }}>{error}</p>}
           <span className="flex">
             <hr className="line" />
             OR
@@ -49,6 +81,7 @@ const LoginForm = ({ setIsLogin }) => {
             />
           </div>
         </div>
+        
       </div>
     );
   };

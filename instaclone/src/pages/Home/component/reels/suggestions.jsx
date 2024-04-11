0ec66f514270a41ad0ca56c7d1a02a59fcd56837
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-11 16:38:51 +0300

    add suggestion

--- instaclone/src/pages/Home/component/reels/suggestions.jsx
+++ instaclone/src/pages/Home/component/reels/suggestions.jsx
@@ -1,9 +1,64 @@
 import React from "react"
+import './suggestions.css'
 
 const Suggestions = ()=>{
     return (
-        <div>
-            
+        <div className="suggestions">
+            <p>Suggestion for you</p>
+            <div className="suggestion-heading">
+                <div className="suggestion-names flex column">
+                    <div className="suggest-name flex ">
+                        <div className="name-info flex">
+                            <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80" alt="" />
+                            <p>Mahmoud</p>
+
+                        </div>
+                        <button className="follow-btn">follow</button>
+                       
+                        
+                    </div>
+                    <div className="suggest-name flex ">
+                        <div className="name-info flex">
+                            <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80" alt="" />
+                            <p>Mahmoud</p>
+
+                        </div>
+                        <button className="follow-btn">follow</button>
+                       
+                        
+                    </div>
+                    <div className="suggest-name flex ">
+                        <div className="name-info flex">
+                            <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80" alt="" />
+                            <p>Mahmoud</p>
+
+                        </div>
+                        <button className="follow-btn">follow</button>
+                       
+                        
+                    </div>
+                    <div className="suggest-name flex ">
+                        <div className="name-info flex">
+                            <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80" alt="" />
+                            <p>Mahmoud</p>
+
+                        </div>
+                        <button className="follow-btn">follow</button>
+                       
+                        
+                    </div>
+                    <div className="suggest-name flex ">
+                        <div className="name-info flex">
+                            <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80" alt="" />
+                            <p>Mahmoud</p>
+
+                        </div>
+                        <button className="follow-btn">follow</button>
+                       
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

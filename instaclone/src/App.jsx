bae44746e7a6668ee40e87732887ba574bf2aa5b
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-11 15:26:17 +0300

    update app

--- instaclone/src/App.jsx
+++ instaclone/src/App.jsx
@@ -3,6 +3,8 @@ import "./styles/uitilities.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Authentication from "./pages/Authentication";
 import Home from "./pages/Home";
+import './index.css'
+import Profile from "./pages/Home/component/Profile/profile";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route  path="/" element={<Authentication />}/>
         <Route  path="/home" element={<Home />}/>
+        <Route  path="/profile" element={<Profile />}/>
       </Routes>
     </BrowserRouter>
       
